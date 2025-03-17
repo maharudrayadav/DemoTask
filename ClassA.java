7a9ae855480ab903eed510ac7d954bd35ccc043d
rudra212001 <maharudrayadav>
2025-03-17 08:01:17 +0530

    Initial commit

---
+++ ClassA.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class ClassA {
+    protected int i=10;
+}
---
+++ ClassB.java
@@ -0,0 +1,12 @@
+package org.example;
+
+ class ClassB extends ClassA{
+
+     int i=10;
+
+    public static void main(String[] args) {
+        ClassB b1=new ClassB();
+        System.out.println(b1.i);
+    }
+}
+
